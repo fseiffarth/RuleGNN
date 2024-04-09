77e5a914601d762b2af070d50fa24aa9f23323f4
florian <seiffarth@cs.uni-bonn.de>
2024-04-09 17:29:34 +0200

    Fixed sh file

--- Experiment.sh
+++ Experiment.sh
@@ -4,12 +4,9 @@ MAX_RUN_ID=2
 CROSS_VALIDATION=9
 CONFIG_FILE="config.yml"
 
-for network_type in $NETWORK_TYPES; do
-  (for i in $(seq 0 $MAX_RUN_ID); do
+for i in $(seq 0 $MAX_RUN_ID); do
       for j in $(seq 0 $CROSS_VALIDATION); do
         python GraphLearningMain.py --graph_db_name $DATABASE_NAME --run_id $i --validation_id $j --config $CONFIG_FILE &
       done
-  done
-  wait)
 done
 
