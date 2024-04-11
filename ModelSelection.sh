26763fc28bfc95d8949ecd7d3bab9a58ef2df280
florian <seiffarth@cs.uni-bonn.de>
2024-04-11 17:14:24 +0200

    no random config

--- ModelSelection.sh
+++ ModelSelection.sh
@@ -4,7 +4,7 @@ CROSS_VALIDATION=9
 CONFIG_FILE="config_NCI1.yml"
 for DATABASE_NAME in $DATABASE_NAMES; do
   for j in $(seq 0 $CROSS_VALIDATION); do
-    python ModelSelection.py --graph_db_name $DATABASE_NAME --run_id 0 --validation_id $j --config $CONFIG_FILE &
+    python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --config $CONFIG_FILE &
   done
 done
 
