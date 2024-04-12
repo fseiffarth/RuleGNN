164406cdd9324725c1e8897d28fb56c1b39f2ab0
florian <seiffarth@cs.uni-bonn.de>
2024-04-12 11:02:29 +0200

    model selection sh update

--- ModelSelection.sh
+++ ModelSelection.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 DATABASE_NAMES="NCI1 NCI109 Mutagenicity"
-CROSS_VALIDATION=9
+CROSS_VALIDATION=10
 CONFIG_FILE="config_NCI1.yml"
 for DATABASE_NAME in $DATABASE_NAMES; do
-  for j in $(seq 0 $CROSS_VALIDATION); do
-    python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --config $CONFIG_FILE &
+  for j in $(seq 1 $CROSS_VALIDATION); do
+    python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --validation_number $CROSS_VALIDATION --config $CONFIG_FILE &
   done
 done
 
