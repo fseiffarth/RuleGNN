622022d9d1212a213b59396061ddfd9e9e30c6f1
florian <seiffarth@cs.uni-bonn.de>
2024-04-18 10:25:29 +0200

    Updated config path

--- ModelSelectionZINC.sh
+++ ModelSelectionZINC.sh
@@ -6,7 +6,7 @@ CROSS_VALIDATION=0
 CONFIG_FILES="config_ZINC_best1.yml config_ZINC_best2.yml config_ZINC_best3.yml config_ZINC_best4.yml config_ZINC_best5.yml config_ZINC_best6.yml config_ZINC_best7.yml config_ZINC_best8.yml config_ZINC_best9.yml config_ZINC_best10.yml config_ZINC_best11.yml config_ZINC_best12.yml config_ZINC_best13.yml config_ZINC_best14.yml config_ZINC_best15.yml"
 for CONFIG_FILE in $CONFIG_FILES; do
   for j in $(seq 0 $CROSS_VALIDATION); do
-    python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --config $CONFIG_FILE &
+    python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --config 'Configs/ZINC/'$CONFIG_FILE &
   done
 done
 
