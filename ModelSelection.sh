b46a286924ecdab2535a3d563ce078e563420d5d
florian <seiffarth@cs.uni-bonn.de>
2024-05-10 14:23:29 +0200

    use 1 threads

--- ModelSelection.sh
+++ ModelSelection.sh
@@ -2,9 +2,7 @@
 DATABASE_NAMES="IMDB-BINARY"
 CROSS_VALIDATION=1
 CONFIG_FILE="Configs/config_IMDB-BINARY_all_speed.yml"
-export MKL_NUM_THREADS=3
-export NUMEXPR_NUM_THREADS=3
-export OMP_NUM_THREADS=3
+export OMP_NUM_THREADS=1
 for DATABASE_NAME in $DATABASE_NAMES; do
   for j in $(seq 0 $CROSS_VALIDATION); do
     python ModelSelection.py --graph_db_name $DATABASE_NAME --validation_id $j --config $CONFIG_FILE &
