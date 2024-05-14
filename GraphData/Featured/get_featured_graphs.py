155da08d9fb7d9d12f2a015b85472fd2b7b5ffa8
florian <seiffarth@cs.uni-bonn.de>
2024-05-14 07:58:44 +0200

    add features to the graphs

--- GraphData/Featured/get_featured_graphs.py
+++ GraphData/Featured/get_featured_graphs.py
@@ -1,4 +1,7 @@
+import os
+
 from GraphData.GraphData import get_graph_data
+from GraphData.Labels.generator.load_labels import load_labels
 from Layers.GraphLayers import Layer
 
 
@@ -9,12 +12,12 @@ def create_dataset(dataset_name):
     graph_data = get_graph_data(dataset_name, data_path, distance_path, use_features=True,use_attributes=False)
 
     layers = [Layer({'layer_type' : 'wl', 'wl_iterations' : 2, 'max_node_labels': 500})]
-
-    label_path = f"GraphData/Labels/{dataset_name}_{l.get_layer_string()}_labels.txt"
-    for label_path in label_paths:
+    for l in layers:
+        label_path = f"GraphData/Labels/{dataset_name}_{l.get_layer_string()}_labels.txt"
         if os.path.exists(label_path):
             g_labels = load_labels(path=label_path)
             graph_data.node_labels[l.get_layer_string()] = g_labels
+
 
 
 def main():
