bb473204d8f46cdd9e7fc26eaa0c748120d027ea
florian <seiffarth@cs.uni-bonn.de>
2024-06-20 11:56:56 +0200

    path update

--- scripts/ModelSelection.py
+++ scripts/ModelSelection.py
@@ -171,7 +171,8 @@ def run_configuration(config_id, run_config, graph_data: GraphData, graph_db_nam
     for l in run_config.layers:
         # add the labels to the graph data
         label_path = f"{l_path}{graph_db_name}_{l.get_layer_string()}_labels.txt"
-        if os.path.exists(label_path):
+        absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        if os.path.exists(absolute_path + "/" + label_path):
             g_labels = load_labels(path=label_path)
             graph_data.node_labels[l.get_layer_string()] = g_labels
         elif l.layer_type == "combined":  # create combined file if it is a combined layer and the file does not exist
