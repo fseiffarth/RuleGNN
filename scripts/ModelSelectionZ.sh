7a87c87fd48640451f2a1db25575dcd31a07eee1
florian <seiffarth@cs.uni-bonn.de>
2024-06-25 11:42:41 +0200

    ZINC update

--- scripts/ModelSelectionZ.sh
+++ scripts/ModelSelectionZ.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DATABASE_NAME="ZINC"
 CROSS_VALIDATION=0
-CONFIG_FILES="Configs/Test/config_ZINC_1.yml Configs/Test/config_ZINC_2.yml Configs/Test/config_ZINC_3.yml Configs/Test/config_ZINC_4.yml Configs/Test/config_ZINC_5.yml Configs/Test/config_ZINC_6.yml Configs/Test/config_ZINC_7.yml Configs/Test/config_ZINC_8.yml Configs/Test/config_ZINC_9.yml Configs/Test/config_ZINC_10.yml Configs/Test/config_ZINC_11.yml Configs/Test/config_ZINC_12.yml Configs/Test/config_ZINC_13.yml Configs/Test/config_ZINC_14.yml Configs/Test/config_ZINC_15.yml"
+CONFIG_FILES="Configs/Test/ZINC/config_ZINC_1.yml Configs/Test/ZINC/config_ZINC_2.yml Configs/Test/ZINC/config_ZINC_3.yml Configs/Test/ZINC/config_ZINC_4.yml Configs/Test/ZINC/config_ZINC_5.yml Configs/Test/ZINC/config_ZINC_6.yml Configs/Test/ZINC/config_ZINC_7.yml Configs/Test/ZINC/config_ZINC_8.yml Configs/Test/ZINC/config_ZINC_9.yml Configs/Test/ZINC/config_ZINC_10.yml Configs/Test/ZINC/config_ZINC_11.yml Configs/Test/ZINC/config_ZINC_12.yml Configs/Test/ZINC/config_ZINC_13.yml Configs/Test/ZINC/config_ZINC_14.yml Configs/Test/ZINC/config_ZINC_15.yml"
 export OMP_NUM_THREADS=1
 for CONFIG_FILE in $CONFIG_FILES; do
   for j in $(seq 0 $CROSS_VALIDATION); do
