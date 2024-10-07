040d966cf73d044f660f420f3d3e6bf7c25e292a
florian <florian.seiffarth@t-online.de>
2024-10-07 22:16:04 +0200

    test positions

--- src/Architectures/RuleGNN/RuleGNNLayers.py
+++ src/Architectures/RuleGNN/RuleGNNLayers.py
@@ -373,6 +373,17 @@ class RuleConvolutionLayer(nn.Module):
             # in weight_array add 1 where the index is in weight_pos
         self.weight_distribution = new_weight_distribution
 
+        # print weight positions to file
+        with open(f"Testing/Multilabel/weight_positions_new.txt", "w") as f:
+            for i, weights in enumerate(self.weight_distribution):
+                weight_pos = weights[:, 3]
+                for pos in weight_pos:
+                    f.write(f"{pos}\n")
+            for i, weights in enumerate(self.bias_distribution):
+                weight_pos = weights[:, 3]
+                for pos in weight_pos:
+                    f.write(f"{pos}\n")
+
         # set 0 entries in self.in_edges to 1 to avoid division by zero
         for i in range(len(self.in_edges)):
             self.in_edges[i][self.in_edges[i] == 0] = 1
@@ -755,6 +766,12 @@ class RuleAggregationLayer(nn.Module):
 
             self.weight_distribution.append(np.array(graph_weight_pos_distribution, dtype=np.int64))
 
+        # print weight positions to file
+        with open(f"Testing/Multilabel/weight_positions_new.txt", "a") as f:
+            for i, weights in enumerate(self.weight_distribution):
+                weight_pos = weights[:, 3]
+                for pos in weight_pos:
+                    f.write(f"{pos}\n")
 
     def weight_map(self, channel, out_dim, label):
         '''
