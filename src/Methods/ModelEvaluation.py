dd60c055a6531c222aaf54bd94ba7cc06e68e2ca
florian <florian.seiffarth@t-online.de>
2024-11-29 10:18:14 +0100

    bugfix random variation

--- src/Methods/ModelEvaluation.py
+++ src/Methods/ModelEvaluation.py
@@ -166,9 +166,9 @@ class ModelEvaluation:
                         std = self.para.run_config.config['input_features']['random_variation'].get('std', 0.1)
                         random_variation = torch.normal(mean=mean, std=std, size=self.graph_data[graph_id].x.size())
                         if self.para.run_config.config.get('precision', 'double') == 'float':
-                            random_variation = torch.tensor(random_variation).float()
+                            random_variation = torch.tensor(random_variation).float().clone().detach()
                         else:
-                            random_variation = torch.tensor(random_variation).double()
+                            random_variation = torch.tensor(random_variation).double().clone().detach()
                         outputs[j] = self.net(self.graph_data[graph_id].x + random_variation, graph_id)
                     else:
                         outputs[j] = self.net(self.graph_data[graph_id].x, graph_id)
