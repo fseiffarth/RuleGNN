f5e062435598c5c6a38aa553440ffa96c1331784
florian <florian.seiffarth@t-online.de>
2025-02-05 21:10:47 +0100

    add node classification

--- src/utils/SyntheticGraphs.py
+++ src/utils/SyntheticGraphs.py
@@ -323,3 +323,25 @@ def first_a(data_size=1500, max_size=40, seed=764,*args, **kwargs) -> (List[nx.G
         valid = rand_sequence[0]
         labels.append(valid)
     return graphs, labels
+
+def node_classification_test(data_size=1, max_size=1000, num_node_features=1,seed=764,*args, **kwargs) -> (List[nx.Graph], List[int]):
+    graphs = []
+    labels = []
+    np.random.seed(seed)
+    for i in range(data_size):
+        size = np.random.randint(1, max_size + 1)
+        G = nx.Graph()
+        for j in range(size):
+            G.add_node(j, label=0)
+        for j in range(size - 1):
+            G.add_edge(j, j + 1)
+        # create random seqeunce of size size of 0s and 1s
+        rand_sequence = np.random.randint(0, 2, size)
+        # assign the labels to the nodes
+        for j in range(size):
+            G.nodes[j]["label"] = rand_sequence[j]
+        graphs.append(G)
+        # check wheter first and last node have the same label 0
+        valid = rand_sequence[0]
+        labels.append(valid)
+    return graphs, labels
