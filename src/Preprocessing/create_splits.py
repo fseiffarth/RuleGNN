67aab75c4515fdd61aa7bb1b9857917b31e168f9
florian <florian.seiffarth@t-online.de>
2025-07-14 23:31:52 +0200

    feat: add ZINC experiment script and update configurations for precision settings

--- src/Preprocessing/create_splits.py
+++ src/Preprocessing/create_splits.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 
 from src.Preprocessing.GraphData.GraphData import get_graph_data, GraphData
-from src.utils.utils import get_train_validation_test_list
+from src.utils.utils import get_train_validation_test_list, get_data_indices
 
 
 def zinc_splits():
@@ -166,7 +166,7 @@ def create_splits(db_name: str, data_path: Path = Path("../GraphData/DS_all/"), 
     if graph_data is None:
         graph_data = get_graph_data(db_name=db_name, data_path=data_path, graph_format=graph_format, only_graphs=True)
 
-    run_test_indices = ttd.get_data_indices(len(graph_data), seed=seed, kFold=folds)
+    run_test_indices = get_data_indices(len(graph_data), seed=seed, kFold=folds)
     for validation_id in range(0, folds):
         validation_seed = seed + validation_id
 
@@ -179,7 +179,7 @@ def create_splits(db_name: str, data_path: Path = Path("../GraphData/DS_all/"), 
         else:
             graph_labels = graph_data.y
 
-        training_data, validate_data, test_data = ttd.get_train_validation_test_list(test_indices=run_test_indices,
+        training_data, validate_data, test_data = get_train_validation_test_list(test_indices=run_test_indices,
                                                                                          validation_step=validation_id,
                                                                                          seed=validation_seed,
                                                                                          balanced=False,
