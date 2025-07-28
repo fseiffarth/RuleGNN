b12413fd8c101889290184f0cf748376fb0cb0d6
fseiffarth <seiffarth@cs.uni-bonn.de>
2025-07-28 16:17:34 +0200

    feat: enhance graph data processing with regression target selection and output normalization

--- paper_experiments/regression/ZINC/main_ZINC.py
+++ paper_experiments/regression/ZINC/main_ZINC.py
@@ -2,22 +2,25 @@
 from pathlib import Path
 
 import click
+import joblib
 
 from src.Experiment.ExperimentMain import ExperimentMain
 
 def main_ZINC(num_threads=-1):
-    experiment = ExperimentMain(Path('paper_experiments/regression/ZINC/configs/main_config_ZINC.yml'))
-    experiment.ExperimentPreprocessing(num_threads=num_threads)
-    ## run real world experiment
-    experiment.GridSearch(num_threads=num_threads)
-    experiment.EvaluateResults()
-    experiment.RunBestModel(num_threads=num_threads)
-    experiment.EvaluateResults(evaluate_best_model=True)
+    for threshold in [2, 5, 10]:
+        experiment = ExperimentMain(Path(f'paper_experiments/regression/ZINC/configs/main_config_ZINC_{threshold}.yml'))
+        experiment.ExperimentPreprocessing(num_threads=num_threads)
+        ## run real world experiment
+        experiment.GridSearch(num_threads=num_threads)
+        experiment.EvaluateResults()
+        experiment.RunBestModel(num_threads=num_threads)
+        experiment.EvaluateResults(evaluate_best_model=True)
 
 @click.command()
 @click.option('--num_threads', default=-1, help='Number of threads to use')
 def main(num_threads):
-    main_ZINC(num_threads)
+    # parallelize over thresholds
+    main_ZINC(num_threads=num_threads)
 
 
 
--- test_scripts/load_labels.py
+++ test_scripts/load_labels.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 import torch
@@ -14,6 +15,32 @@ def load_label_files(path: Path):
 
 
 def main():
+    db = 'ZINC'
+    # get work directory
+    work_dir = Path(sys.argv[0]).resolve().parent.parent
+    label_root_path = Path(f'{work_dir}/paper_experiments/Data/Labels/{db}/')
+    # get all files in the label root path
+    label_files = list(label_root_path.glob(f'{db}_labels_*.pt'))
+    label_data = {}
+    for file in label_files:
+        # get name of the file without the extension
+        file_name = file.stem
+        label_data[file_name] = load_label_files(file)
+
+    # create statistics over the label data as table name, number of unique labels
+    label_statistics = []
+    for name, labels in label_data.items():
+        num_unique_labels = len(torch.unique(labels[2][:,1]))
+        label_statistics.append({
+            'name': name,
+            'num_unique_labels': num_unique_labels
+        })
+    print("Label Statistics:")
+    for stat in label_statistics:
+        print(f"{stat['name']}: {stat['num_unique_labels']} unique labels")
+
+    labels = load_label_files(label_root_path.joinpath(label_path_1))
+
     test_0 = load_label_files('Labels/MUTAG_labels_wl_labeled_simple_cycles_6_6_base_labels_1.pt')
     test_1 = load_label_files('Labels/MUTAG_labels_wl_labeled_simple_cycles_6_6_primary_base_labels_1.pt')
     base_path = Path('/home/florian/Documents/Code/GNNs/RuleGNN/ReproduceExtended/Data/Labels/ZINC/')
