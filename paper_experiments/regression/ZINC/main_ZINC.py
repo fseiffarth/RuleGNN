30350e4a558a8811fd5be1ae9c2ad799a86ec1cc
fseiffarth <seiffarth@cs.uni-bonn.de>
2025-07-29 17:50:55 +0200

    feat: enhance graph data processing with regression target selection and output normalization

--- paper_experiments/regression/ZINC/main_ZINC.py
+++ paper_experiments/regression/ZINC/main_ZINC.py
@@ -7,14 +7,13 @@ import joblib
 from src.Experiment.ExperimentMain import ExperimentMain
 
 def main_ZINC(num_threads=-1):
-    for threshold in [2, 5, 10]:
-        experiment = ExperimentMain(Path(f'paper_experiments/regression/ZINC/configs/main_config_ZINC_{threshold}.yml'))
-        experiment.ExperimentPreprocessing(num_threads=num_threads)
-        ## run real world experiment
-        experiment.GridSearch(num_threads=num_threads)
-        experiment.EvaluateResults()
-        experiment.RunBestModel(num_threads=num_threads)
-        experiment.EvaluateResults(evaluate_best_model=True)
+    experiment = ExperimentMain(Path(f'paper_experiments/regression/ZINC/configs/main_config_ZINC.yml'))
+    experiment.ExperimentPreprocessing(num_threads=num_threads)
+    ## run real world experiment
+    experiment.GridSearch(num_threads=num_threads)
+    experiment.EvaluateResults()
+    experiment.RunBestModel(num_threads=num_threads)
+    experiment.EvaluateResults(evaluate_best_model=True)
 
 @click.command()
 @click.option('--num_threads', default=-1, help='Number of threads to use')
